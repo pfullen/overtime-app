0aa13922930c1d16a35adde179f638c4ee966972
Peter Fullen <peter@fullencpa.com>
2017-06-09 13:17:06 -0400

    Implemented Post policy

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -37,6 +37,11 @@ class ApplicationPolicy
   def scope
     Pundit.policy_scope!(user, record.class)
   end
+ 
+  def admin_types
+    ['AdminUser']
+  end
+
 
   class Scope
     attr_reader :user, :scope
