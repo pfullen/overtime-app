3cf8eb85a2e1b61b2a211a5e30d6b8e86cf9f3ea
Peter Fullen <peter@fullencpa.com>
2017-06-14 13:36:54 -0400

    Installed new pagination styles

--- app/helpers/post1s_helper.rb
+++ app/helpers/post1s_helper.rb
@@ -1,14 +1,5 @@
 module Post1sHelper
 
 
-	def status_label status
-		case status
-		when 'submitted'
-			content_tag(:span, status, class: 'label label-primary')
-		when 'approved'
-			content_tag(:span, status, class: 'label label-success')
-		when 'rejected'
-			content_tag(:span, status, class: 'label label-danger')
-		end
-	end
+	
 end
